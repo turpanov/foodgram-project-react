0d983be7d47b0c10ff78dea799b41c7e2084ab49
Andrey Turpanov <turpanov.a@gmail.com>
2022-08-29 13:04:03 +0300

    settings comments

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #  
+        #  Данные настройки подготовлены для дальнейшего перехода на PostgreSQL.
+        #  Разработка велась на SQLite
+        #
         # 'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         # 'NAME': os.getenv('DB_NAME', default='postgres'),
         # 'USER': os.getenv('POSTGRES_USER', default='postgres'),
