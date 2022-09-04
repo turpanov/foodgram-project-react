e0048f7fd30c12a3207a22ec4a0217c578a6f27c
Andrey Turpanov <turpanov.a@gmail.com>
2022-09-04 18:05:18 +0300

    shopping cart fix

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -8,7 +8,7 @@ def download_csv_shopping_cart(recipe_ingredient):
     ingredients = recipe_ingredient.values(
         'ingredient_id__name', 'ingredient_id__measurement_unit'
     ).annotate(ingredient_amount=Sum('amount')).values_list(
-        'ingredient_id__name', 'amount',
+        'ingredient_id__name', 'ingredient_amount',
         'ingredient_id__measurement_unit',
     )
     response = HttpResponse(content_type='text/csv')
